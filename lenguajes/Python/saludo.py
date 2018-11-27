4db860e5d7cdf87bcc00790874c205fcd764f4ae
cristian <cristianteves1@gmail.com>
2018-11-27 18:36:28 -0300

    .

--- lenguajes/Python/saludo.py
+++
@@ -1,3 +0,0 @@
-nombre = input("Ingrese su nombre \n")
-
-print ("Hola: " + nombre)
--- lenguajes/Python/varonOmujer.py
+++
@@ -1,7 +0,0 @@
-genero = input("Ingrese su genero (M/F): ")
-
-if genero.upper() == "M":
-    print("Es varon")
-else:
-    print("Es mujer")
-
